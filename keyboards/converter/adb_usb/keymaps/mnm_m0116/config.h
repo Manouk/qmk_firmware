063130c7e36df42f354b27f7608eb95287881d22
Manouk <m.manoukian@gmail.com>
2019-09-28 14:35:16 +0100

    ISO addition for adb converter

---
+++ keyboards/converter/adb_usb/keymaps/mnm_m0116/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// place overrides here
+#define TAPPING_TERM 180
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#endif
---
+++ keyboards/converter/adb_usb/keymaps/mnm_m0116/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+#include <print.h>
+
+#define CTL_X   MT(MOD_LCTL, KC_X)
+#define CTL_DOT MT(MOD_RCTL, KC_DOT)
+#define ALT_Z   MT(MOD_LALT, KC_Z)
+#define ALT_SLS MT(MOD_LALT, KC_SLSH)
+
+#define _BL 0
+#define _FL 1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BL] = LAYOUT_m0116_ansi(
+                               LGUI(KC_L),
+    KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,        KC_LNUM, KC_EQL, KC_PSLS, KC_PAST,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                KC_P7,   KC_P8,  KC_P9,   KC_PPLS,
+    KC_BSPC, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,        KC_P4,   KC_P5,  KC_P6,   KC_PMNS,
+    KC_LSFT, ALT_Z,   CTL_X,  KC_C,   KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, CTL_DOT, ALT_SLS,                   KC_RSFT,       KC_P1,   KC_P2,  KC_P3,
+    KC_LCTL, KC_LALT, KC_LGUI, LT(_FL, KC_GRV),        KC_SPC,                  KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,       KC_P0,           KC_PDOT, KC_PENT
+    ),
+
+    [_FL] = LAYOUT_m0116_ansi(
+                               _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  _______, _______, _______, _______,
+    _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______,
+    KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______,
+    _______, _______, _______, _______,                   _______,                   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,          _______, _______
+    ),
+};
+
+void led_set_user(uint8_t usb_led) {
+}
