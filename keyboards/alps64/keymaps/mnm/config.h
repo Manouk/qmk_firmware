11cbe7bfdf6a2078f9f10c1bdb7ce5e204f0f56e
Manouk <m.manoukian@gmail.com>
2019-10-13 11:31:03 +0100

    Alps64 configuration

---
+++ keyboards/alps64/keymaps/mnm/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 175
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#endif
